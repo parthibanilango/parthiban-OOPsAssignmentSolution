bcb6c20065fb3985acb6e05525b3cae504f0f47d
parthibanilango <parthibanilango89@gmail.com>
2021-09-03 20:28:41 +0530

    adds super department print statement

--- src/com/greatlearning/driver/Main.java
+++ src/com/greatlearning/driver/Main.java
@@ -1,6 +1,7 @@
 package com.greatlearning.driver;
 
 import com.greatlearning.department.AdminDepartment;
+import com.greatlearning.department.Department;
 import com.greatlearning.department.HRDepartment;
 import com.greatlearning.department.TechDepartment;
 
@@ -8,6 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Department department=new Department();
 		AdminDepartment adminDepartment = new AdminDepartment();
 		HRDepartment hrDepartment = new HRDepartment();
 		TechDepartment techDepartment = new TechDepartment();
@@ -17,6 +19,8 @@ public class Main {
 		System.out.println(hrDepartment.displayGreetingAndInformation());
 		System.out.println(System.lineSeparator());
 		System.out.println(techDepartment.displayGreetingAndInformation());
+		System.out.println(System.lineSeparator());
+		System.out.println(department.displayGreetingAndInformation());
 
 	}
 
